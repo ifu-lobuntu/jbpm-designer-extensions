785eb9ec0c8a7f63d792df0b5cce82b513ad2cf0
Ampie Barnard <ampieb@gmail.com>
2015-03-05 15:24:10 +0200

    More modularization

--- jbpm-designer-cmmn/jbpm-designer-cmmn-backend/src/test/resources/org/jbpm/designer/public/js/ie-utils-min.js
+++
@@ -1,6 +0,0 @@
-if((typeof Range!=="undefined")&&!Range.prototype.createContextualFragment){Range.prototype.createContextualFragment=function(a){var c=document.createDocumentFragment(),b=document.createElement("div");
-c.appendChild(b);
-b.outerHTML=a;
-return c
-}
-};
